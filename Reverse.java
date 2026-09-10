908fa00407bd749b7232b63890878500bad08a47
nsaksharacse2405a2-ux <nsakshara_cse2405a2@mgit.ac.in>
2026-09-10 18:12:41 +0530

    added reverse string program

---
+++ Reverse.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Reverse
+ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        char[] arr = str.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        System.out.println("Reversed string: " + new String(arr));
+    }
+}
